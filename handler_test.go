fc44ae099dd6d1b0d2bef110f015351736feab6b
Daniel Sage <sagedanielr@gmail.com>
2023-03-09 00:36:10 -0500

    Add notification handler test

--- handler_test.go
+++ handler_test.go
@@ -9,13 +9,14 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 const secret = `hey this is really secret`
 
 func TestSubHandler_ServeHTTP_VerificationBasic(t *testing.T) {
 	handler := NewSubHandler(true, []byte(secret))
-	res := handleVerificationRequest(handler, newVerificationRequest)
+	res := handleRequest(handler, newVerificationRequest)
 	body, _ := io.ReadAll(res.Body)
 	_ = res.Body.Close()
 
@@ -25,7 +26,7 @@ func TestSubHandler_ServeHTTP_VerificationBasic(t *testing.T) {
 
 func TestSubHandler_ServeHTTP_VerificationInvalidSignature(t *testing.T) {
 	handler := NewSubHandler(true, []byte(secret))
-	res := handleVerificationRequest(handler, newBadVerificationRequest)
+	res := handleRequest(handler, newBadVerificationRequest)
 	_ = res.Body.Close()
 
 	assert.Equal(t, res.StatusCode, http.StatusForbidden)
@@ -37,14 +38,14 @@ func TestSubHandler_ServeHTTP_VerificationDynamic(t *testing.T) {
 		return h.SubscriptionType == "channel.update"
 	}
 
-	res := handleVerificationRequest(handler, newVerificationRequestWithType("channel.update"))
+	res := handleRequest(handler, newVerificationRequestWithType("channel.update"))
 	body, _ := io.ReadAll(res.Body)
 	_ = res.Body.Close()
 
 	assert.True(t, isOK(res.StatusCode))
 	assert.Equal(t, body, []byte("olYc8-klwIH9BthhWWhTU-AhJQ0eatixVF2y6x3G5kk"))
 
-	res = handleVerificationRequest(handler, newVerificationRequestWithType("channel.follow"))
+	res = handleRequest(handler, newVerificationRequestWithType("channel.follow"))
 	body, _ = io.ReadAll(res.Body)
 	_ = res.Body.Close()
 
@@ -57,21 +58,34 @@ func TestSubHandler_ServeHTTP_IDTracker(t *testing.T) {
 	handler.IDTracker = tracker
 
 	// First request should succeed
-	res := handleVerificationRequest(handler, newVerificationRequest)
+	res := handleRequest(handler, newVerificationRequest)
 	_ = res.Body.Close()
 
 	assert.True(t, isOK(res.StatusCode))
 	assert.False(t, tracker.DuplicateSeen)
 
 	// Second request should fail as duplicate
-	res = handleVerificationRequest(handler, newVerificationRequest)
+	res = handleRequest(handler, newVerificationRequest)
 	_ = res.Body.Close()
 
 	assert.True(t, isOK(res.StatusCode)) // should still give 2xx code
 	assert.True(t, tracker.DuplicateSeen)
 }
 
-func handleVerificationRequest(handler *SubHandler, reqFactory func() *http.Request) *http.Response {
+func TestSubHandler_ServeHTTP_Notification(t *testing.T) {
+	d := newDispatcher(1)
+	handler := NewSubHandler(false, nil)
+	handler.HandleChannelUpdate = func(h *esb.ResponseHeaders, event *esb.EventChannelUpdate) {
+		d.Trigger()
+	}
+
+	res := handleRequest(handler, newNotificationRequest)
+
+	assert.True(t, isOK(res.StatusCode))
+	assert.True(t, d.WaitForTrigger(100*time.Millisecond), "HandleChannelUpdate failed to trigger")
+}
+
+func handleRequest(handler *SubHandler, reqFactory func() *http.Request) *http.Response {
 	verificationReq := reqFactory()
 	w := httptest.NewRecorder()
 	handler.ServeHTTP(w, verificationReq)
@@ -112,6 +126,25 @@ func newVerificationRequestWithType(typ string) func() *http.Request {
 	}
 }
 
+func newNotificationRequest() *http.Request {
+	bodyData := []byte(`{"subscription":{"id":"ef7e8fba-6c32-4ead-965d-61f21660d095","status":"enabled","type":"channel.update","version":"1","condition":{"broadcaster_user_id":"132532813"},"transport":{"method":"webhook","callback":"https://testing.proxy.b.dnsge.org/webhooks"},"created_at":"2023-03-09T04:44:48.057734342Z","cost":0},"event":{"broadcaster_user_id":"132532813","broadcaster_user_login":"icelys","broadcaster_user_name":"icelys","title":"hello there!","language":"en","category_id":"509658","category_name":"Just Chatting","is_mature":false}}`)
+
+	req := httptest.NewRequest("POST", "/", bytes.NewReader(bodyData))
+	req.Header = http.Header{
+		"Content-Type":                                     {"application/json"},
+		"Twitch-Eventsub-Message-Id":                       {"eTOJ71BBQNXGNW8qPUNMRGIHH5yv4bBrvwl02DWgF0o="},
+		"Twitch-Eventsub-Message-Retry":                    {"0"},
+		"Twitch-Eventsub-Message-Signature":                {"sha256=e6e0d4bc8eb792d9081be6c4fa64416b9a756c5fa4675867f38487f94751f9a6"},
+		"Twitch-Eventsub-Message-Timestamp":                {"2023-03-09T04:45:36.836089549Z"},
+		"Twitch-Eventsub-Message-Type":                     {"notification"},
+		"Twitch-Eventsub-Subscription-Is-Batching-Enabled": {"false"},
+		"Twitch-Eventsub-Subscription-Type":                {"channel.update"},
+		"Twitch-Eventsub-Subscription-Version":             {"1"},
+	}
+
+	return req
+}
+
 func isOK(statusCode int) bool {
 	return 200 <= statusCode && statusCode < 300
 }
@@ -127,4 +160,23 @@ func (w *wrapper) AddAndCheckIfDuplicate(ctx context.Context, id string) (bool, 
 		w.DuplicateSeen = true
 	}
 	return a, e
+}
+
+type dispatcher chan struct{}
+
+func newDispatcher(n int) dispatcher {
+	return make(chan struct{}, n)
+}
+
+func (d dispatcher) Trigger() {
+	d <- struct{}{}
+}
+
+func (d dispatcher) WaitForTrigger(dur time.Duration) bool {
+	select {
+	case <-d:
+		return true
+	case <-time.After(dur):
+		return false
+	}
 }
